6035fc738ab55d975415c1ad472ca4ceb8de2029
yube01 <yubraja008@gmail.com>
2023-12-27 16:49:25 +0545

    home page linked to inventory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Inventory from "./pages/inventory/Inventory";
 import Sales from './pages/sales/Sales';
+import Home from './pages/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
    
     {
       path: "/sales",
